4ba4c62d0b8f308ac2f0ab51bef1659e3bc04f2e
nick rothwell <nick@cassiel.com>
2013-05-28 13:10:00 +0100

    Initial Leiningen project setup.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljs-video-control
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cljs_video_control/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljs-video-control.core-test
+  (:use clojure.test
+        cljs-video-control.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
